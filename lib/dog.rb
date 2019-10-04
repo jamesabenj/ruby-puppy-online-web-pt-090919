16d3b205865e5aa79be99852172bd5bd4a23a318
James Benjamin <james.abenj@gmail.com>
2019-10-04 01:29:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,37 @@
 # Add your code here
+require "pry" 
+class Dog 
+  
+  attr_reader :name 
+  
+  @@all = []
+  
+  def save
+   @@all << self
+  end 
+  
+    
+  def initialize(name)
+    @@name = name
+    save
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+ 
+  def self.print_all 
+    @@all.each do |names|
+      binding.pry 
+      puts names
+      
+    end 
+  end 
+  
+  def self.clear_all
+    @@all.clear 
+  end 
+
+    
+  
+end
